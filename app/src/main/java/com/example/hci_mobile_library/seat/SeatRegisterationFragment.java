66aafd0ce87ada4241c8d07cb9828ac9061c713e
ihh0 <732tein@gmail.com>
2023-06-12 23:44:31 +0900

    add seat registeration fragment
    등록/예약을 한 곳에 통합할 예정

---
+++ app/src/main/java/com/example/hci_mobile_library/seat/SeatRegisterationFragment.java
@@ -0,0 +1,42 @@
+package com.example.hci_mobile_library.seat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.hci_mobile_library.R;
+
+public class SeatRegisterationFragment extends Fragment {
+
+//    FragmentManager fragmentManager = getChildFragmentManager();
+//    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+    public SeatRegisterationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_myseat, container, false);
+
+        Button buttonToFragment1 = view.findViewById(R.id.button_history);
+
+        buttonToFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySeatHistoryFragment historyFragment = new MySeatHistoryFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, historyFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        return view;
+    }
+}
